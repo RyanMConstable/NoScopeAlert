aa0f58184a87ac566673137c4a9e0b89a9fb6c8f
RyanMConstable <RyanMConstable@gmail.com>
2026-04-30 12:35:49 -0400

    Currently collects users latest game codes

--- Components/UpdateGameCodes/Go/checkgamecodes.go
+++ Components/UpdateGameCodes/Go/checkgamecodes.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"time"
 )
 
 func CheckGameCodes(u Users) error {
 	//Here we must check forever the status of the games of users.
 	//Asynchronous will most likely be the best way to do this.
 	for {
-		fmt.Println("Never ending")
+		//Loop through users
+		for _, user := range u.users {
+			gameCodeURL := fmt.Sprintf("https://api.steampowered.com/ICSGOPlayers_730/GetNextMatchSharingCode/v1?key=%v&steamid=%v&steamidkey=%v&knowncode=%v", user.key, user.steamid, user.steamidkey, user.knowncode)
+
+			resp, err := http.Get(gameCodeURL)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			fmt.Println(string(body))
+
+		}
+		time.Sleep(2 * time.Second)
 	}
-	return nil
 }
