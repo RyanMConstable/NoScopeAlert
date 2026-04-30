648fdefe4fa7949209b3479e2ae4409de4088e03
RyanMConstable <RyanMConstable@gmail.com>
2026-04-30 10:16:45 -0400

    Add base for check game codes file

---
+++ Components/UpdateGameCodes/Go/checkgamecodes.go
@@ -0,0 +1,7 @@
+package main
+
+func CheckGameCodes(u Users) error {
+	//Here we must check forever the status of the games of users.
+	//Asynchronous will most likely be the best way to do this.
+	return nil
+}
--- Components/UpdateGameCodes/Go/updategamecodes.go
+++ Components/UpdateGameCodes/Go/updategamecodes.go
@@ -7,9 +7,19 @@ import (
 func main() {
 	var users Users
 
+	//Function to set users
+	//In the future this will be a database grab, and then the loop will sit in memory for ever.
+	//Adding new users must be instant and will have to be thought about as well
+	//For now this is just my user for testing (not included in git so people don't steal my keys)
 	InitUsers(&users)
-
 	fmt.Println(users)
+
+	//Here we must constantly check every user to see if a new game has been played.
+	//If a game has been played, then we must send that code and the steamid to a rabbitmq queue
+	err := CheckGameCodes(users)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 type Users struct {
